d4c60932caafa10fc6bdb6e8eb19938b14aeabfd
三木田　和樹 <72425784+mikitakazuki@users.noreply.github.com>
2020-10-21 17:14:47 +0900

    Add files via upload

---
+++ Project2/Project2/ObjHero.cpp
@@ -0,0 +1,221 @@
+//使用するヘッダーファイル
+#include "GameL\DrawTexture.h"
+#include "GameL\SceneManager.h"
+#include "GameL\HitBoxManager.h"
+#include "GameL\WinInputs.h"
+
+#include "GameHead.h"
+#include "ObjHero.h"
+
+//当たり判定のオブジェクト情報部の数
+#define Collision_detection 1
+//主人公のHP（体力）
+#define HP 5
+//主人公のスピード
+#define SPEED 1.0f
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjHero::Init()
+{
+	m_bullet = true;
+	m_uniquebullet_1 = true;
+	m_uniquebullet_2 = true;
+	//m_stop_flag = true;
+	m_human = false;
+	m_hp = HP;
+
+	//当たり判定
+	Hits::SetHitBox(this, m_x, m_y, 92, 92, ELEMENT_PLAYER, OBJ_HERO, 1);
+}
+
+//アクション
+void CObjHero::Action()
+{
+	////戦車状態：攻撃
+	//if (m_human == false)
+	//{
+	//	//通常弾
+	//	if (Input::GetVKey('C') == true)
+	//	{
+	//		if (m_bullet == true)
+	//		{
+	//			//弾丸オブジェクト作成
+	//			CObjBullet* obj_b = new CObjBullet(m_x, m_y);//弾丸オブジェクト作成
+	//			Objs::InsertObj(obj_b, OBJ_BULLET, 1);//作った弾丸オブジェクトをオブジェクトマネージャーに登録
+
+	//			m_bullet = false;
+	//		}
+	//		else
+	//		{
+	//			m_bullet = true;
+	//		}
+	//	}
+	//	//特殊弾（１）
+	//	else if (Input::GetVKey('X') == true)
+	//	{
+	//		if (m_uniquebullet_1 == true)
+	//		{
+	//			//弾丸オブジェクト作成
+	//			CObjBullet* obj_b = new CObjBullet(m_x, m_y);//弾丸オブジェクト作成
+	//			Objs::InsertObj(obj_b, OBJ_BULLET, 1);//作った弾丸オブジェクトをオブジェクトマネージャーに登録
+
+	//			m_uniquebullet_1 = false;
+	//		}
+	//		else
+	//		{
+	//			m_uniquebullet_1 = true;
+	//		}
+	//	}
+	//	//特殊弾（２）
+	//	else if (Input::GetVKey('Z') == true)
+	//	{
+	//		if (m_uniquebullet_2 == true)
+	//		{
+	//			//弾丸オブジェクト作成
+	//			CObjBullet* obj_b = new CObjBullet(m_x, m_y);//弾丸オブジェクト作成
+	//			Objs::InsertObj(obj_b, OBJ_BULLET, 1);//作った弾丸オブジェクトをオブジェクトマネージャーに登録
+
+	//			m_uniquebullet_2 = false;
+	//		}
+	//		else
+	//		{
+	//			m_uniquebullet_2 = true;
+	//		}
+	//	}
+	//}
+	////人状態：攻撃
+	//if (m_human == true)
+	//{
+	//	//通常弾
+	//	if (Input::GetVKey('C') == true)
+	//	{
+	//		if (m_attack == true)
+	//		{
+
+	//			m_attack = false;
+	//		}
+	//		else
+	//		{
+	//			m_attack = true;
+	//		}
+	//	}
+	//}
+	//戦車状態→人状態へ
+	if (m_human == false)
+	{
+		//主人公が人状態に移行
+		if (Input::GetVKey('V') == true)
+		{
+			if (m_flag == true)
+			{
+				//主人公（人）
+				m_human = true;
+				//グラフィックの破棄
+
+				//グラフィック読み込み
+				Draw::LoadImageW(L"主人公_人.png", 0, TEX_SIZE_512);
+
+			}
+			else
+			{
+				m_flag = true;
+			}
+		}
+	}
+	//人状態→戦車状態へ
+	else if (m_human == true)
+	{
+		//主人公が人状態に移行
+		if (Input::GetVKey('V') == true)
+		{
+			if (m_flag == true)
+			{
+				//主人公（人）
+				m_human = false;
+				//グラフィックの破棄
+
+				//グラフィック読み込み
+				Draw::LoadImageW(L"主人公_戦車.png", 0, TEX_SIZE_512);
+
+			}
+			else
+			{
+				m_flag = true;
+			}
+		}
+	}
+	//右方向
+	if (Input::GetVKey(VK_RIGHT) == true)
+	{
+		m_r = 270.0f;
+		m_x += SPEED;
+	}
+	//左方向
+	if (Input::GetVKey(VK_LEFT) == true)
+	{
+		m_r = 90.0f;
+		m_x -= SPEED;
+	}
+	//上方向
+	if (Input::GetVKey(VK_UP) == true)
+	{
+		m_r = 0.0f;
+		m_y -= SPEED;
+	}
+	//下方向
+	if (Input::GetVKey(VK_DOWN) == true)
+	{
+		m_r = 180.0f;
+		m_y += SPEED;
+	}
+
+	//HitBoxの位置情報更新
+	CHitBox* hit = Hits::GetHitBox(this);
+	hit->SetPos(m_x, m_y);
+
+	//当たり判定を行うオブジェクト情報部
+	int data_base[Collision_detection] =
+	{
+
+	};
+
+	//敵オブジェクトと接触したら主人公のm_hpが減少
+	if (hit->CheckObjNameHit(data_base[Collision_detection]) != nullptr)
+	{
+		m_hp -= 1;
+	}
+	//m_hpが０になると主人公を破棄
+	if (m_hp >= 0)
+	{
+		this->SetStatus(false);//自身に削除命令を出す
+		Hits::DeleteHitBox(this);//主人公が所有するHitBoxを削除する
+	}
+}
+
+//ドロー
+void CObjHero::Draw()
+{
+	//カラー情報
+	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
+
+	RECT_F src;//描画元切り取り位置
+	RECT_F dst;//描画先表示位置
+
+	//切り取り位置の設定
+	src.m_top = 0.0f;
+	src.m_left = 0.0f;
+	src.m_right = 300.0f;
+	src.m_bottom = 300.0f;
+
+	//表示位置の設定
+	dst.m_top = 0.0f + m_y;
+	dst.m_left = 0.0f + m_x;
+	dst.m_right = 32.0f + 64.0f + m_x;
+	dst.m_bottom = 32.0f + 64.0f + m_y;
+
+	//描画
+	Draw::Draw(0, &src, &dst, c, m_r);
+}
---
+++ Project2/Project2/ObjHero.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "GameL\SceneObjManager.h"
+
+using namespace GameL;
+
+class CObjHero :public CObj
+{
+public:
+	CObjHero() {};
+	~CObjHero() {};
+	void Init();//イニシャライズ
+	void Action();//アクション
+	void Draw();//ドロー
+private:
+	int m_hp{ 0 };//主人公のヒットポイント（体力）
+
+	float m_x{ 0 };//主人公移動用変数：横軸
+	float m_y{ 0 };//主人公移動用変数：縦軸
+
+	float m_r{ 0 };//主人公の角度制御用
+
+	bool m_bullet;//主人公（戦車）：通常弾制御用
+	bool m_uniquebullet_1;//主人公（戦車）：特殊弾（１）制御用
+	bool m_uniquebullet_2;//主人公（戦車）：特殊弾（２）制御用
+
+	bool m_flag;//人状態に移行フラグ
+	bool m_human;//主人公（人）制御用
+	bool m_attack;//主人公（人）：攻撃
+};
