5bf9f4365ef13d928a06d6006711613d890ddcc4
三木田　和樹 <h1202019025@hiratagakuen.onmicrosoft.com>
2020-10-24 23:49:36 +0900

    Enemy微調整

--- Project2/Project2/Enemy.cpp
+++ Project2/Project2/Enemy.cpp
@@ -232,8 +232,11 @@ void CObjEnemy::Action()
 	////弾丸と接触しているかを調べる
 	//if (hit->CheckObjNameHit(OBJ_BULLET) != nullptr)
 	//{
-	//	this->SetStatus(false);//自身に削除命令を出す
-	//	Hits::DeleteHitBox(this);//弾丸が所有するHitBoxに削除する。
+	//	m_hp--;
+	//	if (m_hp == 0) {
+	//		this->SetStatus(false);//自身に削除命令を出す
+	//		Hits::DeleteHitBox(this);//弾丸が所有するHitBoxに削除する。
+	//	}
 	//}
 	m_vx = 0;
 	m_vy = 0;
