6986b55d34eeb112e545aab3ce7b74fe1ab19131
聖矢 小川 <h1202019030@hiratagakuen.onmicrosoft.com>
2020-10-26 01:26:23 +0900

    a

---
+++ Project2/Project2/Objblock.h
@@ -0,0 +1,22 @@
+#pragma once
+//使用するヘッダー
+#include "GameL\SceneObjManager.h"
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト・箱：背景
+class CObjBlock :public CObj
+{
+public:
+	CObjBlock() {};
+	~CObjBlock() {};
+	void Init();
+	void Action();
+	void Draw();
+private:
+	int m_map[80][60];//マップ情報（仮）
+
+	float m_scroll;//上下左右スクロール用
+
+};
+
